53b2d88611461f84ef68b803aa096330e796bf03
Hector Arciga <hellektor@git.gutter>
2013-12-17 07:16:15 -0600

    Initial Commit

---
+++ textinputsource/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  textinputsource
+//
+//  Created by Hector Arciga on 12/17/13.
+//  Copyright (c) 2013 Hector Arciga. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
